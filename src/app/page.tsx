a3982f5366d6e693b101cc339086777117d78586
rinz <roberts.denisovs@gmail.com>
2024-08-10 23:38:13 +0300

    yeah

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,10 @@ import Link from "next/link";
 export default function Home() {
   return ( 
     <SessionProvider>
-      <div className="-z-10 text-sm text-neutral-400 fixed flex items-end h-screen p-8" >
+      <div className=" text-sm text-neutral-400 fixed flex items-end h-screen p-8" >
         Like this project on&nbsp;<Link href="https://github.com/rinzexe/aura-wizard">github</Link>&nbsp;so that I can get hired
       </div>
-      <div className="w-screen min-h-dvh h-full gap-12 flex flex-col justify-center items-center">
+      <div className="w-screen z-10 min-h-dvh h-full gap-12 flex flex-col justify-center items-center">
           <div className="grid p-4 lg:grid-rows-1  gap-12 grid-rows-2 lg:grid-cols-2 w-full max-w-[80rem]">
             <LeaderBoard />
             <ProfilePanel />
